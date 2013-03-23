aba44907a7e31f7ad1f1fdf991707a7c30f5fdd1
Peter Mårtensson <veritas.is.me@gmail.com>
2013-03-23 12:31:43 +0100

    uypdate
    Signed-off-by: Peter Mårtensson <veritas.is.me@gmail.com>

--- SomniumTest/main.cpp
+++ SomniumTest/main.cpp
@@ -6,10 +6,15 @@ using SomniumEngine::System::Engine;
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
             char *cmd, int cmdline)
 {
+  if(!Engine::Initialize())
+  {
+    MessageBox(NULL, "Failed to initialize SomniumEngine!"
+      "\nPlease see Engine.log for more information!", "ERROR",
+      MB_OK | MB_ICONERROR);
+  }
+
   Engine *engine = Engine::GetInstance();
-  if(engine->InitializeSystem(SubSystemWindows) &&
-     engine->InitializeSystem(SubSystemGraphics) && engine->Run()) ;
-  else
+  if(!engine->Run())
   {
     MessageBox(NULL, "SomniumEngine has exited with an error!"
       "\nPlease see Errors.log and Engine.log for more information!", "ERROR",
